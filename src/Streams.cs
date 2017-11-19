d6e3a6d1bbcdc6c5b50a55ce3f5c13cfeeb76388
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2017-11-19 16:35:39 -0500

    Addition of Stream extensions

---
+++ src/Streams.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace JotunShard.Extensions
+{
+    public static class Streams
+    {
+        public static IEnumerable<string> ReadLines(this TextReader reader)
+        {
+            reader.CheckArgumentNull(nameof(reader));
+            for (string line; ((line = reader.ReadLine()) != null);)
+                yield return line;
+        }
+
+        public static async Task<string[]> ReadLinesAsync(this TextReader reader)
+        {
+            IEnumerable<Task<string>> GetAsyncLines()
+            {
+                for (Task<string> line; ((line = reader.ReadLineAsync()) != null);)
+                    yield return line;
+            }
+
+            reader.CheckArgumentNull(nameof(reader));
+            return await Task.WhenAll(GetAsyncLines());
+        }
+
+        public static void WriteLines(this TextWriter writer, IEnumerable<string> lines)
+        {
+            writer.CheckArgumentNull(nameof(writer));
+            lines.CheckArgumentNull(nameof(lines));
+            foreach (var line in lines)
+                writer.WriteLineAsync(line);
+        }
+
+        public static async Task WriteLinesAsync(this TextWriter writer, IEnumerable<string> lines)
+        {
+            writer.CheckArgumentNull(nameof(writer));
+            lines.CheckArgumentNull(nameof(lines));
+            foreach (var line in lines)
+                await writer.WriteLineAsync(line);
+        }
+    }
+}
