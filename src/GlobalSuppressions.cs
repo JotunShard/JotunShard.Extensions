e063a65f5c343b36a0664f39343396fbb9f62ca6
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2017-11-21 20:13:50 -0500

    Suppressing of S2436

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Sonar Code Smell", "S2436:Classes and methods should not have too many generic parameters", Justification = "<En attente>", Scope = "member", Target = "~M:JotunShard.Extensions.Delegates.Bind``3(System.Func{``0,``1,``2},``0,``1)~System.Func{``2}")]
+
