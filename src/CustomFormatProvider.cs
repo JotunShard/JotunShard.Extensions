65ffeb4e602c55669be6e926dd846900b96452ca
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2018-03-09 20:51:16 -0500

    Addition of formatting utilities

---
+++ src/CustomFormatProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace JotunShard.Extensions
+{
+    public abstract class CustomFormatProvider : IFormatProvider, ICustomFormatter
+    {
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            return null;
+        }
+
+        public abstract string Format(string format, object arg, IFormatProvider formatProvider);
+    }
+}
