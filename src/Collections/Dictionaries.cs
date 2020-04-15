b52d784b36a519812ffb9019668103cff62cb8d2
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2020-04-15 19:44:36 -0400

    Added Join, GroupJoin and ToDictionary for KeyValuePair enumerables

--- src/Collections/Dictionaries.cs
+++ src/Collections/Dictionaries.cs
@@ -1,4 +1,5 @@
 ﻿using JetBrains.Annotations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -25,5 +26,35 @@ namespace JotunShard.Extensions
             KeyValuePair<TKey, TValue> pair = default(KeyValuePair<TKey, TValue>))
             => source.DefaultIfEmpty(pair.Key, pair.Value);
 
+        public static IEnumerable<KeyValuePair<TKey, TResult>> Join<TKey, TValue, TResult>(
+            [NotNull] this IEnumerable<KeyValuePair<TKey, TValue>> outer,
+            [NotNull] IEnumerable<KeyValuePair<TKey, TValue>> inner,
+            [NotNull] Func<TKey, TValue, TValue, KeyValuePair<TKey, TResult>> resultSelector,
+            EqualityComparer<TKey> keyComparer = null)
+            => outer.Join(inner, o => o.Key, i => i.Key,
+                (o, i) => resultSelector(o.Key, o.Value, i.Value), keyComparer);
+
+        public static IEnumerable<KeyValuePair<TKey, TResult>> GroupJoin<TKey, TValue, TResult>(
+            [NotNull] this IEnumerable<KeyValuePair<TKey, TValue>> outer,
+            [NotNull] IEnumerable<KeyValuePair<TKey, TValue>> inner,
+            [NotNull] Func<TKey, TValue, IEnumerable<TValue>, KeyValuePair<TKey, TResult>> resultSelector,
+            EqualityComparer<TKey> keyComparer = null)
+            => outer.GroupJoin(inner, o => o.Key, i => i.Key,
+                (o, i) => resultSelector(o.Key, o.Value, i.Select(ii => ii.Value)), keyComparer);
+
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(
+            [NotNull] this IEnumerable<KeyValuePair<TKey, TValue>> source,
+            IEqualityComparer<TKey> comparer = null)
+        {
+            if (!(source is Dictionary<TKey, TValue> dict))
+            {
+                return source.ToDictionary(kv => kv.Key, kv => kv.Value, comparer);
+            }
+            if (comparer != null && !dict.Comparer.Equals(comparer))
+            {
+                dict = new Dictionary<TKey, TValue>(dict, comparer);
+            }
+            return dict;
+        }
     }
 }
