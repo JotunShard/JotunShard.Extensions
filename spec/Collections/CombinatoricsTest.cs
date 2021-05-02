ab96479717381a173ae35f4b38bf735b8f15c822
CodeBeast357 <270547+CodeBeast357@users.noreply.github.com>
2021-05-01 20:46:46 -0400

    Added combinatorics

---
+++ spec/Collections/CombinatoricsTest.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace JotunShard.Extensions.Test.Collections
+{
+    using static Assert;
+
+    public class CombinatoricsTest
+    {
+        [Fact]
+        public void ToAllCombinations_WithNullEnumerable_Throws()
+            => Throws<ArgumentNullException>(
+                () => ((IOrderedEnumerable<int>)null).ToAllCombinations().ToList());
+
+        [Fact]
+        public void ToAllCombinations_WithSizeOf0_ExpectsAllCombinations()
+        {
+            var source = new int[0];
+            var expected = new int[0][];
+            var actual = source.ToAllCombinations().ToList();
+            Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToAllCombinations_WithSizeOf1_ExpectsAllCombinations()
+        {
+            var source = new[] { 1 };
+            var expected = new[]
+            {
+                new[]{ 1 }
+            };
+            var actual = source.ToAllCombinations().ToList();
+            Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToAllCombinations_WithSizeOf2_ExpectsAllCombinations()
+        {
+            var source = new[] { 1, 2 };
+            var expected = new[]
+            {
+                new[]{ 1 },
+                new[]{ 2 },
+                new[]{ 1, 2 }
+            };
+            var actual = source.ToAllCombinations().ToList();
+            Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToAllCombinations_WithSizeOf3_ExpectsAllCombinations()
+        {
+            var source = new[] { 1, 2, 3 };
+            var expected = new[]
+            {
+                new[]{ 1 },
+                new[]{ 2 },
+                new[]{ 1, 2 },
+                new[]{ 3 },
+                new[]{ 1, 3 },
+                new[]{ 1, 2, 3 },
+                new[]{ 2, 3 }
+            };
+            var actual = source.ToAllCombinations().ToList();
+            Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ToAllCombinations_WithSizeOf4_ExpectsAllCombinations()
+        {
+            var source = new[] { 1, 2, 3, 4 };
+            var expected = new[]
+            {
+                new[]{ 1 },
+                new[]{ 2 },
+                new[]{ 1, 2 },
+                new[]{ 3 },
+                new[]{ 1, 3 },
+                new[]{ 1, 2, 3 },
+                new[]{ 2, 3 },
+                new[]{ 4 },
+                new[]{ 1, 4 },
+                new[]{ 1, 2, 4 },
+                new[]{ 2, 4 },
+                new[]{ 1, 3, 4 },
+                new[]{ 1, 2, 3, 4 },
+                new[]{ 2, 3, 4 },
+                new[]{ 3, 4 }
+            };
+            var actual = source.ToAllCombinations().ToList();
+            Equal(expected, actual);
+        }
+    }
+}
---
+++ src/Collections/Combinatorics.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JotunShard.Extensions
+{
+    public static class Combinatorics
+    {
+        public static IEnumerable<IEnumerable<TElem>> ToAllCombinations<TElem>(
+            this IEnumerable<TElem> source)
+        {
+            source.CheckArgumentNull(nameof(source));
+            using (var enmrtr = source.GetEnumerator())
+            {
+                if (!enmrtr.MoveNext()) yield break;
+                var firstElement = enmrtr.Current;
+                if (!enmrtr.MoveNext())
+                {
+                    yield return source;
+                    yield break;
+                }
+                var secondElement = enmrtr.Current;
+                var oldCombinations = new List<IEnumerable<TElem>>();
+                var firstPassDone = false;
+                var secondPassDone = false;
+                while (true)
+                {
+                    var element = firstPassDone
+                        ? secondPassDone
+                            ? enmrtr.Current
+                            : secondElement
+                        : firstElement;
+                    var combinationOfOne = new[] { element };
+                    yield return combinationOfOne;
+                    var newCombinations = new List<IEnumerable<TElem>>();
+                    foreach (var combination in oldCombinations)
+                    {
+                        var newCombination = combination.Union(combinationOfOne);
+                        yield return newCombination;
+                        newCombinations.Add(newCombination);
+                    }
+                    oldCombinations.AddRange(newCombinations);
+                    oldCombinations.Add(combinationOfOne);
+                    if (firstPassDone)
+                    {
+                        secondPassDone = true;
+                    }
+                    else if (!secondPassDone)
+                    {
+                        firstPassDone = true;
+                        continue;
+                    }
+                    if (!enmrtr.MoveNext())
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
